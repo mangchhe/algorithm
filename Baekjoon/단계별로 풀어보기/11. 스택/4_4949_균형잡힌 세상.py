0dd2b496df8c1d1f96caf1f1d5dbf4ea5031012b
HaJooHyun <mangchhe@naver.com>
2020-03-03 18:28:40 +0900

    /단계별로 풀어보기/스택 [2문제 완료(10773, 9012), 1문제 진행중(4949)]

---
+++ Baekjoon/단계별로 풀어보기/11. 스택/4_4949_균형잡힌 세상.py
@@ -0,0 +1,54 @@
+'''
+※ 입력
+하나 또는 여러줄에 걸쳐서 문자열이 주어진다.
+각 문자열은 영문 알파벳, 공백, 소괄호("( )") 대괄호("[ ]")등으로 이루어져 있으며, 길이는 100글자보다 작거나 같다.
+
+입력의 종료조건으로 맨 마지막에 점 하나(".")가 들어온다.
+
+※ 출력
+각 줄마다 해당 문자열이 균형을 이루고 있으면 "yes"를, 아니면 "no"를 출력한다.
+'''
+
+import sys
+
+msg = []
+
+while True:
+
+    tmp = list(sys.stdin.readline().rstrip())
+    if len(tmp) == 1 and tmp[0] == '.':
+        break
+    msg.append(tmp)
+
+for i in range(len(msg)):
+    stack = []
+    flag = True
+    for j in msg[i]:
+        if j == '(':
+            stack.append('(')
+        elif j == '[':
+            stack.append('[')
+        elif j == ']':
+            try:
+                if stack[-1] == '[':
+                    del stack[-1]
+                else:
+                    flag = False
+                    break
+            except IndexError:
+                flag = False
+                break
+        elif j == ')':
+            try:
+                if stack[-1] == '(':
+                    del stack[-1]
+                else:
+                    flag = False
+                    break
+            except IndexError:
+                flag = False
+                break
+    if flag:
+        print('yes')
+    else:
+        print('no')
