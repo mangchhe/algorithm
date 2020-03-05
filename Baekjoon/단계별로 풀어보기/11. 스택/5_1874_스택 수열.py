5d5cdd2cc8de9a3f0794bc37913fd7f52cbd151b
HaJooHyun <mangchhe@naver.com>
2020-03-05 14:06:07 +0900

    /단계별로 풀어보기/스택 [1문제 완료(1874)]

--- Baekjoon/단계별로 풀어보기/11. 스택/5_1874_스택 수열.py
+++ Baekjoon/단계별로 풀어보기/11. 스택/5_1874_스택 수열.py
@@ -12,21 +12,72 @@ import sys
 
 stack = []
 last = 1
+result = []
 
 for i in range(int(input())):
 
     N = int(sys.stdin.readline().strip())
 
-    if last <= N:
-        for j in range(last, N+1):
-            print('+')
+    if N >= last:
+        for i in range(last, N+1):
             stack.append(i)
-
-            if j == N:
-                print('-')
-                last = j
-                del stack[-1]
+            result.append('+')
+        last = stack[-1] + 1
+        del stack[-1]
+        result.append('-')
     else:
-        for j in range(last, N-1, -1):
-            print('-')
-            del stack[-1]
+        try:
+            for i in range(len(stack) - stack.index(N)):
+                del stack[-1]
+                result.append('-')
+        except:
+            del result
+            break
+
+try:
+    for i in result:
+        print(i)
+except:
+    print('NO')
+
+# 동국대 NAVER
+
+from sys import stdin
+from collections import deque
+'''
+deque 는 스택과 큐의 기능을 모두 가진 객체
+※ 메서드 종류
+1. append()
+2. appendleft()
+3. pop()
+4. popleft()
+5. extend()
+6. extendleft()
+7. insert()
+8. remove()
+9. reverse()
+'''
+a = list(map(int, stdin.read().split()))[1:]
+'''
+sys.stdin.readline : 띄어쓰기와 \n까지 포함
+sys.stdin : 여러줄 입력 (^Z 시 종료)
+'''
+r = 1
+q = deque()
+res = []
+for x in a:
+    while x >= r:
+        q.append(r)
+        res.append('+')
+        r += 1
+    if q[-1] == x:
+        res.append('-')
+        q.pop()
+    else:
+        print('NO')
+        exit()
+print('\n'.join(res))
+'''
+split 문자열 -> 리스트
+join 리스트 -> 문자열
+'''
