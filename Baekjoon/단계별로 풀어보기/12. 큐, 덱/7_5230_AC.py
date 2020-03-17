25add1597d5028902995011e9e832fd0b3e4d058
HaJooHyun <mangchhe@naver.com>
2020-03-17 16:25:33 +0900

    /단계별로 풀어보기/큐, 덱 [1문제 진행중(5230)]

--- Baekjoon/단계별로 풀어보기/12. 큐, 덱/7_5230_AC.py
+++ Baekjoon/단계별로 풀어보기/12. 큐, 덱/7_5230_AC.py
@@ -5,6 +5,8 @@
 from collections import deque
 from sys import stdin
 
+''' 시간초과
+
 for i in range(int(input())):
 
     cls = list(stdin.readline())
@@ -27,3 +29,47 @@ for i in range(int(input())):
         print(list(tmp))
     else:
         print('error')
+
+'''
+
+for i in range(int(input())): # 틀림(반례 찾는중)
+
+    cls = list(stdin.readline())
+    stdin.readline()
+    rvs = 0
+    err_code = False
+    try:
+        tmp = deque(list(stdin.readline().strip('[]\n').split(',')))
+        if tmp[0] == '':
+            raise IndexError
+    except:
+        err_code = True
+
+    for val in cls:
+        if val == 'R':
+           rvs += 1
+        elif val == 'D':
+            try:
+                if rvs % 2 == 0:
+                    tmp.popleft()
+                else:
+                    tmp.pop()
+            except:
+                err_code = True
+                break
+    if err_code:
+        print('error')
+        continue
+
+    if rvs % 2 == 0:
+        pass
+    else:
+        tmp.reverse()
+
+    print('[',end='')
+    for i in range(len(tmp)):
+        if(i==len(tmp)-1):
+            print(tmp[i],end='')
+        else:
+            print(tmp[i]+',',end='')
+    print(']')
