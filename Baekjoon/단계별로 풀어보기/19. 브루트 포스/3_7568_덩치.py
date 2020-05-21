bc9b9a3c1d7156b5b35c7d9d36ea10ccf3888898
HaJooHyun <mangchhe@naver.com>
2020-05-21 13:46:34 +0900

    /단계별로 풀어보기/브루트 포스 [1문제 완료(7568)]

--- Baekjoon/단계별로 풀어보기/19. 브루트 포스/3_7568_덩치.py
+++ Baekjoon/단계별로 풀어보기/19. 브루트 포스/3_7568_덩치.py
@@ -3,36 +3,33 @@ from sys import stdin
 N = int(stdin.readline())
 
 NList = []
-
 NList_rank = []
 
 for i in range(N):
+    NList.append(list(stdin.readline().split()))
 
-    NList.append(list(map(int, stdin.readline().split())))
+for i in range(N):
 
-NList_rank.append(1)
+    rank = 1
 
-for i in range(1, len(NList)):
+    for j in range(N):
 
-    for j in range(1, len(NList)+1):
+        if j == i:
 
-        try:
-            idx = NList_rank.index(j)
-            if NList[i][0] > NList[idx][0] and NList[i][1] > NList[idx][1]:
-                for k in range(len(NList_rank)):
-                    NList_rank[k] += 1
-                NList_rank.append(j)
-                break
-            elif NList[i][0] < NList[idx][0] and NList[i][1] < NList[idx][1]:
-                if j == max(NList_rank):
-                    NList_rank.append(j + NList_rank.count(j))
-                    break
-            else:
-                NList_rank.append(j)
-                break
-        except:
             pass
 
-for val in NList_rank:
+        else:
 
-    print(val, end=' ')
+            if NList[i][0] < NList[j][0] and NList[i][1] < NList[j][1]:
+
+                rank += 1
+
+            else:
+
+                pass
+
+    NList_rank.append(rank)
+
+for i in NList_rank:
+
+    print(i, end=' ')
