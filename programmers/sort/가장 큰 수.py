42a111a2fa95565cc41c7e416180d5d245e4226c
HaJooHyun <mangchhe@naver.com>
2020-09-09 13:25:01 +0900

    프로그래머스, 가장 큰수 진행중

--- programmers/sort/가장 큰 수.py
+++ programmers/sort/가장 큰 수.py
@@ -1,15 +1,18 @@
 """ 
     작성일 : 20/09/08 - 진행중
+    수정일 : 20/09/09 - 진행중
 """
 
 from itertools import permutations
 import heapq
 
-""" def solution(numbers):
+""" 20/09/08
+    def solution(numbers):
 
     return max(map(lambda x: ''.join(x), permutations(map(str, numbers)))) """
 
-""" def solution(numbers):
+""" 20/09/08
+    def solution(numbers):
 
     numbers = list(map(str, numbers))
     heapq.heapify(numbers)
@@ -19,6 +22,24 @@ import heapq
 
     return result """
 
+def solution(numbers):
 
+    numbers = list(map(str, numbers))
+    resultList = []
+    count = 0
+    maxVal = 0
+    for i in numbers:
+        count += 1
+        tmp = len(i)
+        if tmp > maxVal:
+            maxVal = tmp
+        resultList.append([i,tmp])
+    for i in range(count):
+        resultList[i][0] += resultList[i][0][resultList[i][1] - 1] * (maxVal - resultList[i][1])
+    resultList = sorted(resultList, reverse=True)
+    for i in range(count):
+        resultList[i][0] = resultList[i][0][0:resultList[i][1]]
+    
+    return ''.join(list(zip(*resultList))[0])
 
-print(solution([6, 10, 2, 1]))
+print(solution([2,20,200]))
