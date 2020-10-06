13ffe43d9e1412dcec23ac433f8c2a64535e9608
HaJooHyun <mangchhe@naver.com>
2020-10-06 15:21:00 +0900

    속도 테스트, reverse 잘못된 속도 비교 수정

--- 속도 테스트/reverse.py
+++ 속도 테스트/reverse.py
@@ -1,7 +1,11 @@
 """ 
     제목 : sorted(reverse) vs reversed vs reverse
     작성일 : 20/10/05
-    결과 : reverse > reversed > sorted(reverse)
+    수정일 : 20/10/06
+    reverse > reversed > sorted(reverse) X
+    reverse 와 sorted(reverse) 는 전혀 다른 기능이여서 속도 비교가 불가능
+    reverse : 단순히 리스트를 뒤집는 함수
+    sorted(reverse) : 내림차순으로 리스트를 정렬하는 함수    
 """
 
 import random
