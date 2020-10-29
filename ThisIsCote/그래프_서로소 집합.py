b99e171d1472932279e249289c52efb91a859538
HaJooHyunDesktop <mangchhe@naver.com>
2020-10-29 18:59:36 +0900

    이것이 코테, 개선된 서로소 집합 예제 완료

--- ThisIsCote/그래프_서로소 집합.py
+++ ThisIsCote/그래프_서로소 집합.py
@@ -2,7 +2,11 @@
     작성일 : 20/10/29
 """
 
-def find_parent(parent, x):
+""" 
+    ! 서로소 집합
+"""
+
+""" def find_parent(parent, x):
     if parent[x] != x:
         return find_parent(parent, parent[x])
     return x
@@ -32,3 +36,41 @@ print()
 
 for i in range(1, v + 1):
     print(parent[i], end = ' ')
+"""
+
+""" 
+    ! 개선된 서로소 집합
+    - 루트 노드 찾는 과정 O(V) 해결
+"""
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+v, e = map(int, input().split())
+
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
