439bce33ef9c3ee265d08d314dc4e850a0bf1bb1
HaJooHyunDesktop <mangchhe@naver.com>
2020-10-30 12:54:16 +0900

    이것이 코테, 위상 정렬 예제 완료

---
+++ ThisIsCote/그래프_위상정렬.py
@@ -0,0 +1,37 @@
+"""
+    작성일 : 20/10/30
+"""
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+v, e = map(int, input().split())
+graph = [[] for i in range(v + 1)]
+indegree = [0] * (v + 1)
+result = []
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+def topologySort():
+    q = deque()
+
+    for i in range(1, v + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+topologySort()
+
+for i in result:
+    print(i, end=' ')
