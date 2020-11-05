6b84b38a7a24285aee3e3a4fd40b924bf31bea23
HaJooHyunDesktop <mangchhe@naver.com>
2020-11-05 15:02:18 +0900

    이것이 코테, 볼링공 고르기 문제 수정

--- ThisIsCote/그리드/볼링공 고르기.py
+++ ThisIsCote/그리드/볼링공 고르기.py
@@ -6,10 +6,19 @@ from itertools import combinations
 
 m, n = map(int, input().split())
 data = list(map(int, input().split()))
-data = list(combinations(data, 2))
+""" data = list(combinations(data, 2))
 result = 0
 for i in data:
     if i[0] != i[1]:
         result += 1
+
+print(result) """
+
+result = 0
+
+for i in range(0, m - 1):
+    for j in range(i + 1, m):
+        if data[i] != data[j]:
+            result += 1
 
 print(result)
