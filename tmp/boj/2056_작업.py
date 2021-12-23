e727702c9b058509af5b73a675017991df022437
joohyun ha <mylifeforcoding@gmail.com>
2021-12-23 13:10:28 +0900

    백준: 위상정렬 2문제 완료 - 게임 개발 - 작업

---
+++ tmp/boj/2056_작업.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+# 정석 
+# def solve():
+#     q = deque([])
+#     for i in range(1, N + 1):
+#         if not indegree[i]:
+#             q.append(i)
+#     while q:
+#         n = q.popleft()
+#         for i in graph[n]:
+#             dp[i] = max(dp[i], dp[n] + dis[i])
+#             indegree[i] -= 1
+#             if not indegree[i]:
+#                 q.append(i)
+
+# N = int(input())
+
+# dis = [0] * (N + 1)
+# indegree = [0] * (N + 1)
+# dp = [0] * (N + 1)
+# graph = [[] for _ in range(N + 1)]
+
+# for i in range(1, N + 1):
+#     v = list(map(int, input().split()))
+#     dis[i] = v[0]
+#     dp[i] = v[0]
+#     for j in range(2, len(v)):
+#         graph[v[j]].append(i)
+#         indegree[i] += 1
+
+# solve()
+
+# print(max(dp))
+
+# 개선 방법
+# K번 째 작업이 1 ~ K - 1 의 작업들로만 선행 관계를 가지기 때문에 가능
+input = __import__('sys').stdin.readline
+
+N = int(input())
+dp = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    dp[i], *indegree = map(int, input().split())
+    if indegree[0] > 0: 
+        dp[i] += max(dp[d] for d in indegree[1:])
+
+print(max(dp))
